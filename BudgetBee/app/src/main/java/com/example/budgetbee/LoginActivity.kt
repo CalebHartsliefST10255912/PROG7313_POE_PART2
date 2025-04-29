b483a019e65ab61d8d18fa84262dbcd52c7f2c59
ST10258101 <130571344+ST10258101@users.noreply.github.com>
2025-04-29 12:45:51 +0200

    push 4 (ui update)
    updated ui  flow now you can go from splash to home via other pages

--- BudgetBee/app/src/main/java/com/example/budgetbee/LoginActivity.kt
+++ BudgetBee/app/src/main/java/com/example/budgetbee/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.example.budgetbee
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish() // prevent going back to login screen
+        }
     }
 }
--- BudgetBee/app/src/main/java/com/example/budgetbee/WelcomeActivity.kt
+++ BudgetBee/app/src/main/java/com/example/budgetbee/WelcomeActivity.kt
@@ -1,13 +1,26 @@
 package com.example.budgetbee
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
-
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val buttonSignUp = findViewById<Button>(R.id.buttonSignUp)
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonSignUp.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
