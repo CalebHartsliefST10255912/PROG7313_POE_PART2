7350b672f5ef7e0de9849007149d65446d8c44b2
ST10258256 <130571591+ST10258256@users.noreply.github.com>
2025-04-29 13:06:37 +0200

    Backend Code for Welcome Page
    Backend code to take the user to the login and register page from the welcome page

--- BudgetBee/app/src/main/java/com/example/budgetbee/WelcomeActivity.kt
+++ BudgetBee/app/src/main/java/com/example/budgetbee/WelcomeActivity.kt
@@ -1,13 +1,33 @@
 package com.example.budgetbee
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.budgetbee.R.*
 
 
 class WelcomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(layout.activity_welcome)
+
+        //Values for the Welcome page buttons
+        val loginButton = findViewById<Button>(id.buttonLogin)
+        val registerButton = findViewById<Button>(id.buttonSignUp)
+
+        //ClickListener to take user to Login Page
+        loginButton.setOnClickListener(){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        //ClickListener to take user to Register Page
+        registerButton.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
